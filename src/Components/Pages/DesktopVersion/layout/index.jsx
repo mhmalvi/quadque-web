6137c2439a69e5a19df20f4d8f242ecd533292fe
souraavqq <sourav@quadque.digital>
2022-12-22 10:54:46 +0600

    project structure setup

--- src/Components/Pages/DesktopVersion/layout/index.jsx
+++ src/Components/Pages/DesktopVersion/layout/index.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Layout = () => {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-full">
       {/* Outer Layer */}
-      <div className="w-full h-10 bg-white"></div>
+      <div className="w-10"></div>
+      <div>
+        <div className="w-full h-10 bg-red-400"></div>
         <div className="w-full h-full bg-green-500 main_container"></div>
+        <div className="w-full h-10 bg-blue-400"></div>
+      </div>
     </div>
   );
 };
