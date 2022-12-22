00cf5a5a5d378ad2c073d42861ed3bbc5a271bcc
souraavqq <sourav@quadque.digital>
2022-12-22 10:28:53 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Quadque Tech</h1>
+      <h2>Quadque Tech</h2>
+    </div>
+  );
+}
+
+export default App;
