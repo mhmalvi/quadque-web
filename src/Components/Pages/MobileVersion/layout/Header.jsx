688ea8e33d531ea37dc9510babe9e49bcf588ddb
fahimfayazQQ <fahim@quadque.digital>
2022-12-28 11:27:07 +0600

    Project Form page mobile

--- src/Components/Pages/MobileVersion/layout/Header.jsx
+++ src/Components/Pages/MobileVersion/layout/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
     <div className="w-full mt-30">
       <div className="w-2/3 text-white text-3xl font-semibold px-6">We <br /> Build <br /> Awesome <br /> Creation</div>
       <div className="text-white px-6 pt-3">Contact <span className="bg-white rounded-full text-black px-1 mx-1"> ^ </span></div>
-      <div className="video-opacity-90 pt-10">
-        <video /* autoPlay */ muted >
+      <div className="video-opacity-10 pt-10">
+{/*         <video autoPlay loop muted >
             <source src={Astro} type="video/mp4"/>
-        </video>
+        </video> */}
       </div>
       <div className="flex justify-between bg-black text-white pt-10 pb-20 px-6">
         <div className="font-semibold">START PROJECT</div>
