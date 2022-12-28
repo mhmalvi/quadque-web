c254eb0729ae346236733e9c26f603b40ee07aa3
fahimfayazQQ <fahim@quadque.digital>
2022-12-28 15:12:32 +0600

    network setup mobile

---
+++ src/Components/Pages/MobileVersion/layout/BlogGallery.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogGallery = () => {
+  return (
+    <div>BlogGallery</div>
+  )
+}
+
+export default BlogGallery
--- src/Components/Pages/MobileVersion/layout/StartProject.jsx
+++ src/Components/Pages/MobileVersion/layout/StartProject.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Input, message } from "antd";
 
 const StartProject = () => {
+  const [toogleSubService, setToogleSubService] = useState();
+  const [toogleService, setToogleService] = useState();
   const [data, setData] = useState({
     name: "",
     phone: "",
@@ -21,28 +23,79 @@ const StartProject = () => {
     alert("Your form is submitted.");
   };
 
+  const handleToogleService = (index) => {
+    setToogleService(index);
+  };
+
+    const handleToogleSubService = (index) => {
+    setToogleSubService(index);
+  };
+
   return (
     <div className="w-full text-white mt-20 mb-5">
       <div className="text-3xl px-6 font-bold uppercase pb-8">
         START <br /> &nbsp; PROJECT
       </div>
       <div className="flex flex-wrap justify-between capitalize pb-8 px-6">
-        <p className="px-5">UI & UX</p>
-        <p className="px-5">AI & IoT Solutions</p>
-        <p className="px-5">Digital Marketing</p>
-        <p className="px-5">App Development</p>
-        <p className="px-5">Web Development</p>
+        <p
+          onClick={() => {
+            handleToogleService(1);
+          }}
+          className={`px-5 duration-300 ${toogleService === 1 ? "text-brand-color": ""}`}
+        >
+          UI & UX
+        </p>
+        <p
+          onClick={() => {
+            handleToogleService(2);
+          }}
+          className={`px-5 duration-300 ${toogleService === 2 ? "text-brand-color": ""}`}
+        >
+          AI & IoT Solutions
+        </p>
+        <p
+          onClick={() => {
+            handleToogleService(3);
+          }}
+          className={`px-5 duration-300 ${toogleService === 3 ? "text-brand-color": ""}`}
+        >
+          Digital Marketing
+        </p>
+        <p
+          onClick={() => {
+            handleToogleService(4);
+          }}
+          className={`px-5 duration-300 ${toogleService === 4 ? "text-brand-color": ""}`}
+        >
+          App Development
+        </p>
+        <p
+          onClick={() => {
+            handleToogleService(5);
+          }}
+          className={`px-5 duration-300 ${toogleService === 5 ? "text-brand-color": ""}`}
+        >
+          Web Development
+        </p>
       </div>
 
       <div className="flex flex-wrap justify-between capitalize pb-8 px-6 gap-3">
-        <div className="border rounded-full px-5 py-1">
+        <div onClick={() => {
+            handleToogleSubService(1);
+          }} className={`border rounded-full px-5 py-1 text-semibold duration-300 ${toogleSubService === 1 ?  "bg-white text-black" : ""}`}>
           <p>Digital Marketing</p>
         </div>
-        <div className="border rounded-full px-5 py-1">Google ADs</div>
-        <div className="border rounded-full px-5 py-1">
+        <div onClick={() => {
+            handleToogleSubService(2);
+          }} className={`border rounded-full px-5 py-1 text-semibold duration-300 ${toogleSubService === 2 ?  "bg-white text-black" : ""}`}>Google ADs</div>
+        <div onClick={() => {
+            handleToogleSubService(3);
+          }} className={`border rounded-full px-5 py-1 text-semibold duration-300 ${toogleSubService === 3 ?  "bg-white text-black" : ""}`}>
           social media Marketing
         </div>
-        <div className="border rounded-full px-5 py-1">
+        <div onClick={() => {
+            handleToogleSubService(4);
+          }} className={`border rounded-full px-5 py-1 text-semibold duration-300 ${toogleSubService === 4 ?  "bg-white text-black" : ""}`}>
           Specific Apps marketing
         </div>
       </div>
@@ -106,7 +159,10 @@ const StartProject = () => {
       </form>
 
       <div className="flex justify-end text-sm mx-6">
-        <p>+61405899496 <br />info@quadque.tech</p>
+        <p>
+          +61405899496 <br />
+          info@quadque.tech
+        </p>
       </div>
     </div>
   );
