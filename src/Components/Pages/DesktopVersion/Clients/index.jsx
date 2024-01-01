ba690aeebae3fee3d137a0fd0fd5a83d3b218d7f
ZulkerQQ <zulker@quadque.tech>
2024-01-01 12:24:36 +0600

    !Update: Client section fix

--- src/Components/Pages/DesktopVersion/Clients/index.jsx
+++ src/Components/Pages/DesktopVersion/Clients/index.jsx
@@ -3,41 +3,7 @@ import CountUp from "react-countup";
 import { Fade } from "react-reveal";
 import { useLocation } from "react-router-dom";
 import Lottie from "lottie-react";
-import logo_six from "../../../../assets/Images/ClientLogos/6.png";
-import Aestheticars_logo from "../../../../assets/Images/ClientLogos/Aestheticars_logo.png";
-import agro from "../../../../assets/Images/ClientLogos/agro.png";
-import atr from "../../../../assets/Images/ClientLogos/atr.png";
-import aia from "../../../../assets/Images/ClientLogos/aia.png";
-import alpha from "../../../../assets/Images/ClientLogos/ALPHA.png";
-import bright from "../../../../assets/Images/ClientLogos/BRIGHT.png";
-import clenovative from "../../../../assets/Images/ClientLogos/clenovative.png";
-import dt from "../../../../assets/Images/ClientLogos/dt.png";
-import Epiker from "../../../../assets/Images/ClientLogos/Epiker.png";
-import esg from "../../../../assets/Images/ClientLogos/esg.png";
-import esi from "../../../../assets/Images/ClientLogos/ESI_Logo.png";
-import gb from "../../../../assets/Images/ClientLogos/gb.png";
-import harbour from "../../../../assets/Images/ClientLogos/Harbour.png";
-import ibm from "../../../../assets/Images/ClientLogos/IBM-LOGO.png";
-import itec from "../../../../assets/Images/ClientLogos/ITEC.png";
-import just_for_mom from "../../../../assets/Images/ClientLogos/just_for_mom.png";
-import momola from "../../../../assets/Images/ClientLogos/Momola.png";
-import nta from "../../../../assets/Images/ClientLogos/NTA.png";
-import Officemate from "../../../../assets/Images/ClientLogos/Officemate.png";
-import otobi from "../../../../assets/Images/ClientLogos/otobi.png";
-import paper from "../../../../assets/Images/ClientLogos/paper.png";
-import sa from "../../../../assets/Images/ClientLogos/SA.png";
-import Tanaz from "../../../../assets/Images/ClientLogos/Tanaz.png";
-import Thermottle from "../../../../assets/Images/ClientLogos/Thermottle.png";
-import Top_Trip_Logo from "../../../../assets/Images/ClientLogos/Top_Trip_Logo.png";
-import Trade_Trade_US_Log from "../../../../assets/Images/ClientLogos/Trade_Trade_US_Log.png";
-import trastec from "../../../../assets/Images/ClientLogos/trastec.png";
-import uct from "../../../../assets/Images/ClientLogos/UCT.png";
-import usbangla from "../../../../assets/Images/ClientLogos/usbangla.png";
-import Zaker_Dairy from "../../../../assets/Images/ClientLogos/Zaker_Dairy.png";
-import zdsl from "../../../../assets/Images/ClientLogos/zdsl.png";
-import uniqloAlpha from "../../../../assets/Images/ClientLogos/uniqlo_alpha.png";
-import crush from "../../../../assets/Images/ClientLogos/Crush.png";
-import drobbo from "../../../../assets/Images/ClientLogos/drobbo.png";
+import { Column1xl } from "./data";
 import loaderFile from "../../../../assets/Lotties/loader.json";
 
 const Cleints = () => {
@@ -121,104 +87,13 @@ const Cleints = () => {
         <div className=" hidden lg:hidden xl:hidden 2xl:block">
           <div className="flex items-center mb-4">
             <div>
-              {/* <p className="marquee">
-                <span>
-                  {colum12xl?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[150px] h-[100px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-[100px] flex justify-center items-center">
-                        <img
-                          className="w-8/12 client_img"
-                          src={client}
-                          alt="Clinet"
-                        />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
-              {/* <p className="marquee marquee2">
-                <span>
-                  {colum12xl?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[150px] h-[100px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
+             
             </div>
             <div className="ml-40 pl-5 middle_column">
-              {/* <p className="marquee-reverse">
-                <span>
-                  {colum12xl2?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[150px] h-[100px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
-              {/* <p className="marquee-reverse marquee-reverse-2">
-                <span>
-                  {colum32xl?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[150px] h-[100px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
+             
             </div>
             <div className="ml-40 pl-[24px]">
-              {/* <p className="marquee">
-                <span>
-                  {colum12xl2?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[150px] h-[100px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
-              {/* <p className="marquee marquee2">
-                <span>
-                  {colum32xl?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[150px] h-[100px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
+            
             </div>
           </div>
         </div>
@@ -228,7 +103,7 @@ const Cleints = () => {
             <div>
               <p className="marquee mb-10 ">
                 <span className="my-5">
-                  {colum1xl?.map((client, i) => (
+                  {Column1xl?.map((client, i) => (
                     <div
                       key={i}
                       className="inline-block max-w-[160px] h-[104px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md "
@@ -241,41 +116,13 @@ const Cleints = () => {
                   &nbsp;
                 </span>
               </p>
-              {/* <p className="marquee marquee2">
-                <span>
-                  {colum1xl2?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[160px] h-[104px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
+            
             </div>
             <div className="ml-40 pl-[24px] middle_column">
-              {/* <p className="marquee-reverse">
+       
+              <p className="marquee-reverse">
                 <span>
-                  {colum1xl2?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[160px] h-[104px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
-              <p className="marquee-reverse marquee-reverse-2">
-                <span>
-                  {colum1xl?.map((client, i) => (
+                  {Column1xl?.map((client, i) => (
                     <div
                       key={i}
                       className="inline-block max-w-[160px] h-[104px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
@@ -292,7 +139,7 @@ const Cleints = () => {
             <div className="ml-40 pl-[24px]">
               <p className="marquee">
                 <span>
-                  {colum1xl?.map((client, i) => (
+                  {Column1xl?.map((client, i) => (
                     <div
                       key={i}
                       className="inline-block max-w-[160px] h-[104px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
@@ -305,21 +152,7 @@ const Cleints = () => {
                   &nbsp;
                 </span>
               </p>
-              {/* <p className="marquee marquee2">
-                <span>
-                  {colum1xl?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[160px] h-[104px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
+       
             </div>
           </div>
         </div>
@@ -327,100 +160,12 @@ const Cleints = () => {
         <div className="ml-auto hidden lg:block xl:hidden 2xl:hidden">
           <div className="flex items-center mb-4">
             <div>
-              {/* <p className="marquee">
-                <span>
-                  {colum1lg?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[160px] h-[104px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p>
-              <p className="marquee marquee2">
-                <span>
-                  {colum1lg?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[160px] h-[104px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
+           
             </div>
             <div className="ml-40 pl-[24px] middle_column">
-              {/* <p className="marquee-reverse">
-                <span>
-                  {colum1lg?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[160px] h-[104px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p>
-              <p className="marquee-reverse marquee-reverse-2">
-                <span>
-                  {colum1lg?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[160px] h-[104px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
             </div>
             <div className="ml-40 pl-[24px]">
-              {/* <p className="marquee">
-                <span>
-                  {colum1lg?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[160px] h-[104px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
-              {/* <p className="marquee marquee2">
-                <span>
-                  {colum1lg?.map((client, i) => (
-                    <div
-                      key={i}
-                      className="inline-block max-w-[160px] h-[104px] bg-gradient-to-br from-gray-900 to-gray-600 bg-opacity-30 backdrop-filter backdrop-blur-md border border-gray-500 border-opacity-50 rounded-md"
-                    >
-                      <div className="w-full h-full flex justify-center items-center">
-                        <img className="w-8/12" src={client} alt="Clinet" />
-                      </div>
-                    </div>
-                  ))}
-                  &nbsp;
-                </span>
-              </p> */}
+            
             </div>
           </div>
         </div>
@@ -453,109 +198,4 @@ const Cleints = () => {
 
 export default Cleints;
 
-const colum12xl = [
-  itec,
-  logo_six,
-  Aestheticars_logo,
-  agro,
-  aia,
-  alpha,
-  bright,
-  clenovative,
-  dt,
-  Epiker,
-  esg,
-  Zaker_Dairy,
-  gb,
-  harbour,
-  ibm,
-];
 
-const colum12xl2 = [
-  atr,
-  just_for_mom,
-  momola,
-  nta,
-  Officemate,
-  otobi,
-  paper,
-  sa,
-  Tanaz,
-  Thermottle,
-  Top_Trip_Logo,
-  Trade_Trade_US_Log,
-  trastec,
-  uct,
-  usbangla,
-];
-
-const colum32xl = [
-  uniqloAlpha,
-  logo_six,
-  Aestheticars_logo,
-  agro,
-  aia,
-  alpha,
-  crush,
-  clenovative,
-  dt,
-  Epiker,
-  esg,
-  drobbo,
-  gb,
-  trastec,
-  ibm,
-];
-
-// const colum12xl = [ms, ts, sp, ms, ts, sp, ms, ts, sp, ms, ts, sp, ms, ts, sp];
-const colum1xl = [
-  itec,
-  nta,
-  paper,
-  uct,
-  Zaker_Dairy,
-  zdsl,
-  Thermottle,
-  alpha,
-  ibm,
-  aia,
-  Trade_Trade_US_Log,
-  usbangla,
-  Aestheticars_logo,
-  harbour,
-  trastec,
-  crush,
-  clenovative,
-  dt,
-  Epiker,
-  esg,
-  drobbo,
-  gb,
-  logo_six,
-  uniqloAlpha,
-  Tanaz,
-  Top_Trip_Logo,
-  sa,
-  otobi,
-  paper,
-];
-
-const colum1xl2 = [nta, usbangla, alpha, ibm];
-// const colum1xl = [ms, ts, sp, ms, ts, sp, ms, ts, sp, ms, ts, sp];
-
-const colum1lg = [
-  itec,
-  nta,
-  Thermottle,
-  uct,
-  Zaker_Dairy,
-  esi,
-  Trade_Trade_US_Log,
-  aia,
-  alpha,
-];
-
-// const colum1lg = [ms, ts, sp, ms, ts, sp, ms, ts, sp];
-
-// const colum2 = [ts, sp, ms, ts, sp, ms, ts, sp, ms];
-// const colum3 = [sp, ts, ms, sp, ts, ms, sp, ts, ms];
