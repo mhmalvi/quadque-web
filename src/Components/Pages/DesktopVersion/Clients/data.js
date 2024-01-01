010d00a0eb1022fef53cb2c96413ae63d144f214
ZulkerQQ <zulker@quadque.tech>
2024-01-01 12:24:36 +0600

    !Update: Client section fix

---
+++ src/Components/Pages/DesktopVersion/Clients/data.js
@@ -0,0 +1,67 @@
+import logo_six from "../../../../assets/Images/ClientLogos/6.png";
+import Aestheticars_logo from "../../../../assets/Images/ClientLogos/Aestheticars_logo.png";
+import agro from "../../../../assets/Images/ClientLogos/agro.png";
+import atr from "../../../../assets/Images/ClientLogos/atr.png";
+import aia from "../../../../assets/Images/ClientLogos/aia.png";
+import alpha from "../../../../assets/Images/ClientLogos/ALPHA.png";
+import bright from "../../../../assets/Images/ClientLogos/BRIGHT.png";
+import clenovative from "../../../../assets/Images/ClientLogos/clenovative.png";
+import dt from "../../../../assets/Images/ClientLogos/dt.png";
+import Epiker from "../../../../assets/Images/ClientLogos/Epiker.png";
+import esg from "../../../../assets/Images/ClientLogos/esg.png";
+import esi from "../../../../assets/Images/ClientLogos/ESI_Logo.png";
+import gb from "../../../../assets/Images/ClientLogos/gb.png";
+import harbour from "../../../../assets/Images/ClientLogos/Harbour.png";
+import ibm from "../../../../assets/Images/ClientLogos/IBM-LOGO.png";
+import itec from "../../../../assets/Images/ClientLogos/ITEC.png";
+import just_for_mom from "../../../../assets/Images/ClientLogos/just_for_mom.png";
+import momola from "../../../../assets/Images/ClientLogos/Momola.png";
+import nta from "../../../../assets/Images/ClientLogos/NTA.png";
+import Officemate from "../../../../assets/Images/ClientLogos/Officemate.png";
+import otobi from "../../../../assets/Images/ClientLogos/otobi.png";
+import paper from "../../../../assets/Images/ClientLogos/paper.png";
+import sa from "../../../../assets/Images/ClientLogos/SA.png";
+import Tanaz from "../../../../assets/Images/ClientLogos/Tanaz.png";
+import Thermottle from "../../../../assets/Images/ClientLogos/Thermottle.png";
+import Top_Trip_Logo from "../../../../assets/Images/ClientLogos/Top_Trip_Logo.png";
+import Trade_Trade_US_Log from "../../../../assets/Images/ClientLogos/Trade_Trade_US_Log.png";
+import trastec from "../../../../assets/Images/ClientLogos/trastec.png";
+import uct from "../../../../assets/Images/ClientLogos/UCT.png";
+import usbangla from "../../../../assets/Images/ClientLogos/usbangla.png";
+import Zaker_Dairy from "../../../../assets/Images/ClientLogos/Zaker_Dairy.png";
+import zdsl from "../../../../assets/Images/ClientLogos/zdsl.png";
+import uniqloAlpha from "../../../../assets/Images/ClientLogos/uniqlo_alpha.png";
+import crush from "../../../../assets/Images/ClientLogos/Crush.png";
+import drobbo from "../../../../assets/Images/ClientLogos/drobbo.png";
+
+export const Column1xl = [
+    itec,
+    nta,
+    paper,
+    uct,
+    Zaker_Dairy,
+    zdsl,
+    Thermottle,
+    alpha,
+    ibm,
+    aia,
+    Trade_Trade_US_Log,
+    usbangla,
+    Aestheticars_logo,
+    harbour,
+    trastec,
+    crush,
+    clenovative,
+    dt,
+    Epiker,
+    esg,
+    drobbo,
+    gb,
+    logo_six,
+    uniqloAlpha,
+    Tanaz,
+    Top_Trip_Logo,
+    sa,
+    otobi,
+    paper,
+  ];
